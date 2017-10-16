78af1f72f0c146b2ef9b9a38208885abb591064f
unnikked <unnikked@gmail.com>
2017-10-16 17:22:50 +0200

    Each restyle

--- resources/views/page/each.blade.php
+++ resources/views/page/each.blade.php
@@ -1,2 +1,17 @@
-<a href="{{ route('page.show', $page->id) }}" class="list-group-item" style="overflow: hidden; text-overflow: ellipsis;">
-  <img class="img-circle" src="https://{{ parse_url($page->url, PHP_URL_HOST) }}/favicon.ico" width="16" /> {{ empty($page->title) ? $page->url : $page->title }}<span class="pull-right text-muted">{{ $page->created_at->diffForHumans() }}</span></a>
+<a href="{{ route('page.show', $page->id) }}" class="list-group-item" >
+  <div class="row">
+    <div class="container-fluid ">
+      <div class="col-xs-1">
+        <img class="img-circle" src="https://{{ parse_url($page->url, PHP_URL_HOST) }}/favicon.ico" width="16"/>
+      </div>
+      <div class="col-xs-8">
+        <p style="overflow: hidden;text-overflow: ellipsis;">
+          {{ empty($page->title) ? $page->url : $page->title }}
+        </p>
+      </div>
+      <div class="col-xs-2">
+        <span class="text-muted">{{ $page->created_at->diffForHumans() }}</span>
+      </div>
+    </div>
+  </div>
+</a>
