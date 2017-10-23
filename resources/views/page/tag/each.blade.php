5e21a2aeed68fd86370a57d28125c9bd18b79b7b
unnikked <unnikked@gmail.com>
2017-10-23 18:04:30 +0200

    Restyle Tag

--- resources/views/page/tag/each.blade.php
+++ resources/views/page/tag/each.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ route('tag.show', $tag->id)}}"class="label label-default">{{ $tag->name }}</a>
+<a href="{{ route('tag.show', $tag->id)}}"class="btn btn-default btn-xs">{{ $tag->name }}</a>
