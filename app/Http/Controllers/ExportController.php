1134cd8746cefb014f3ff9919f7a3775f81f5dd4
unnikked <unnikked@gmail.com>
2017-10-23 16:21:50 +0200

    Export rewrite

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use File;
+use App\Http\Resources\Page as PageResource;
+
+// use File;
 
 class ExportController extends Controller
 {
   public function __invoke(Request $request)
   {
 
-    $urls = $request->user()->pages()->get(['url']);
+    // $urls = $request->user()->pages()->get(['url']);
+    //
+    // $filename = '/tmp/' . $request->user()->id . '.json';
+    //
+    // File::put($filename, $urls->toJson());
+    //
+    // return response()->download($filename);
 
-    $filename = '/tmp/' . $request->user()->id . '.json';
-
-    File::put($filename, $urls->toJson());
-
-    return response()->download($filename);
+    return PageResource::collection($request->user()->pages()->with('comments')->get());
   }
 }
