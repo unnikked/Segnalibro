127ffe7e1cee06082712aebaa04a9f99d5d1e481
unnikked <unnikked@gmail.com>
2017-10-25 16:02:47 +0200

    Comment restyle

--- resources/views/page/comment/each.blade.php
+++ resources/views/page/comment/each.blade.php
@@ -1,20 +1,29 @@
-<div class="panel panel-warning">
+<div class="panel panel-default">
   <div class="panel-body">
     <div class="row">
       <div class="col-xs-10">
         {{ $comment->text }}
 
-        @if ($comment->page)
-          {{ $comment->page->id }}
-        @endif
+
       </div>
       <div class="col-xs-2">
-        @component('components.cancellable', ['class' => 'btn btn-danger btn-xs pull-right'])
+        @component('components.cancellable', ['class' => 'btn btn-default btn-xs pull-right'])
           @slot('route')
             {{ route('page.comment.destroy', [$comment->page->id, $comment->id])}}
           @endslot
           Delete
         @endcomponent
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <a href="{{ route('page.show', $comment->page->id)}}">
+            @if (Route::currentRouteName() == 'page.show')
+              Top
+            @else
+              {{ empty($comment->page->title) ? $comment->page->url : $comment->page->title }}
+            @endif
+        </a>
       </div>
     </div>
   </div>
