fe29e89ac505a8d32d54efdd7d6c6e0721e9fcc6
unnikked <unnikked@gmail.com>
2017-10-27 16:04:20 +0200

    Better routing

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,27 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/docs', 'DocController@index')->middleware('auth');
-Route::get('/docs/{id}', 'DocController@show')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+  Route::resource('page', 'PageController');
 
-Route::resource('page', 'PageController')->middleware('auth');
-Route::resource('page.comment', 'CommentController')->middleware('auth');
-Route::get('/comments', 'CommentController@index')->middleware('auth');
-Route::post('/comments/search', 'CommentSearchController')->middleware('auth')->name('comment.search');
-Route::resource('page.tag', 'TagController');
-Route::get('/tag/{tag}', 'TagController@show')->name('tag.show');
+  Route::resource('page.comment', 'CommentController');
+  Route::get('/comments', 'CommentController@index');
+  Route::post('/comments/search', 'CommentSearchController')
+    ->name('comment.search');
 
-Route::post('/page/search', 'PageSearchController')->name('page.search');
+  Route::resource('page.tag', 'TagController');
+  Route::get('/tag/{tag}', 'TagController@show')
+    ->name('tag.show');
 
-Route::get('/export', 'ExportController')->name('export')->middleware('auth');
+  Route::post('/page/search', 'PageSearchController')
+    ->name('page.search');
 
-Route::view('/passport', 'passport.index')->name('passport.index');
+  Route::get('/export', 'ExportController')
+    ->name('export');
+
+  Route::view('/passport', 'passport.index')
+    ->name('passport.index');
+});
+
+Route::get('/docs', 'DocController@index');
+Route::get('/docs/{id}', 'DocController@show');
